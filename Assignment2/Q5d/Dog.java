f703fcda27d9f0525df1aacdb0234a9970e00a28
LuckyTiger8 <102882205+LuckyTiger8@users.noreply.github.com>
2023-03-09 15:19:20 -0800

    Add files via upload

---
+++ Assignment2/Q5d/Dog.java
@@ -0,0 +1,9 @@
+
+public class Dog {
+	private String breed, name;
+	
+	public Dog(){
+		this.breed = "";
+		this.name = "";
+	}
+}
---
+++ Assignment2/Q5d/Paw.java
@@ -0,0 +1,10 @@
+
+public class Paw {
+	private Dog dog;
+	private int position;
+	
+	public Paw(Dog dog, int position) {
+		this.dog = dog;
+		this.position = position;
+	}
+}
