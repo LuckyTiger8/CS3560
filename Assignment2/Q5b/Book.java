d9332bdfe455b2abd218d814424ba597d1737e14
LuckyTiger8 <102882205+LuckyTiger8@users.noreply.github.com>
2023-03-09 15:17:27 -0800

    Add files via upload

---
+++ Assignment2/Q5b/Book.java
@@ -0,0 +1,11 @@
+
+public class Book {
+	private String name, author;
+	private Course course;
+	
+	public Book() {
+		this.name = "";
+		this.author = "";
+		this.course = null;
+	}
+}
---
+++ Assignment2/Q5b/Course.java
@@ -0,0 +1,11 @@
+
+public class Course {
+	private int code;
+	private Book book;
+	
+	public Course(int code, Book book) {
+		this.code = code;
+		this.book = book;
+	}
+	
+}
