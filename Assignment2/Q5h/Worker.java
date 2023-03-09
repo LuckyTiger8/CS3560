0f7e787af7987a63e953102d50dc67dd9bde6b61
LuckyTiger8 <102882205+LuckyTiger8@users.noreply.github.com>
2023-03-09 15:22:16 -0800

    Add files via upload

---
+++ Assignment2/Q5h/Worker.java
@@ -0,0 +1,10 @@
+
+public class Worker {
+	private String name;
+	private double hourlyRate;
+	
+	public Worker(String name, double hourlyRate) {
+		this.name = name;
+		this.hourlyRate = hourlyRate;
+	}
+}
