78aff8ea8b21dac4897f05c5a06a1e94949be539
LuckyTiger8 <102882205+LuckyTiger8@users.noreply.github.com>
2023-03-09 15:16:33 -0800

    Add files via upload

---
+++ Assignment2/Q5a/Student.java
@@ -0,0 +1,11 @@
+
+public class Student {
+	private String name;
+	private Transcript transcript;
+	
+	public Student(String name, Transcript transcript) {
+		this.name = name;
+		this.transcript = transcript;
+	}
+	
+}
