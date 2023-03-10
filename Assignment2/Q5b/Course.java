b966e1204c1a121d63de48bb0c07b58464fbddbc
LuckyTiger8 <102882205+LuckyTiger8@users.noreply.github.com>
2023-03-10 12:10:08 -0800

    Update Course.java

--- Assignment2/Q5b/Course.java
+++ Assignment2/Q5b/Course.java
@@ -1,7 +1,11 @@
-
 public class Course {
 	private int code;
 	private Book book;
+	
+	public Course(int code) {
+		this.code = code;
+		this.book = null;
+	}
 	
 	public Course(int code, Book book) {
 		this.code = code;
